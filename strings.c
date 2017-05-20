dc3afc4a51bf89871b141548c5e964ab5d62b49b
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-20 18:16:51 +0000

    updated return of strncmp to be 0 upon equal strings

--- strings.c
+++ strings.c
@@ -20,7 +20,8 @@ int _strlen(char *s)
  * @s2: second string
  * @n: number of bytes to compare
  *
- * Return: difference of first chars of diff value or 0 on success
+ * Return: integer less than, equal to, or greater than zero if s1 is found,
+ *         respectively, to be less than, to match, or be greater than s2
  */
 int _strncmp(char *s1, char *s2, unsigned int n)
 {
@@ -32,8 +33,8 @@ int _strncmp(char *s1, char *s2, unsigned int n)
 			return (s1[i] - s2[i]);
 
 		if (s1[i] == '\0' || s2[i] == '\0')
-			return (EXT_SUCCESS)
+			return (0)
 	}
 
-	return (EXT_SUCCESS);
+	return (0);
 }
