a2683cdd38be482eca2b7feef6e06b689c05d13b
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-21 21:50:03 +0000

    updated error messages

--- monty.c
+++ monty.c
@@ -17,29 +17,19 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
 		return (EXIT_FAILURE);
 	}
 	filename = argv[1];
 	file = fopen(filename, "r");
-        if (file == NULL)
+	if (file == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s", filename);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
 		return (EXIT_FAILURE);
 	}
 	while ((retval = getline(&line, &n, file)) != -1)
 	{
 		printf("%s", line);
 	}
-
-/**
- * (1) call read file function to read monty bytecode file
- *     -handle errors for no file, etc...
- * (2) add codes to a new stack that we need to create
- *     -handle errors for unknown codes, etc...
- * (3) sort through new stack of codes, and use the execute function to
- *     -handle errors for improper argument or no args for the given opcode
- * (4) call and execute each code one at a time while sorting through list
- * (5) after reading all input codes, terminate successfully
- */
 	return (EXIT_SUCCESS);
 }
