466c8fb065f4760e6eb8b3d1631a92956aa80828
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-22 20:58:43 +0000

    fixed malloc errors and leaked memory for normal inputs

--- built_inventory.c
+++ built_inventory.c
@@ -9,9 +9,9 @@ int build_inventory()
 {
 	inventory = malloc(sizeof(inventory_t));
 	inventory->input = malloc(sizeof(char *) * 3);
-	inventory->stack = malloc(sizeof(stack_t));
+	inventory->stack = NULL;
 
-	if (!inventory || !inventory->input || !inventory->stack)
+	if (!inventory || !inventory->input)
 		handle_errors("malloc fail");
 
 	inventory->linenum = 0;
